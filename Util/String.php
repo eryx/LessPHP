6d29a49c8bbbfa1fa4c4314b2635d0d5e1622052
Eryx <evorui@gmail.com>
2013-07-23 22:57:58 +0800

    [NEW] Util/String

--- Util/String.php
+++ Util/String.php
@@ -1,36 +1,4 @@
 <?php
-/**
- * Hooto Web library
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @category   LessPHP
- * @package    LessPHP_Util_String
- * @copyright  Copyright 2013 HOOTO.COM
- * @license    http://www.apache.org/licenses/LICENSE-2.0
- */
- 
-/** ensure this file is being included by a parent file */
-defined('SYS_ROOT') or die('Access Denied!');
-
-/**
- * Class LessPHP_Util_String
- *
- * @category   LessPHP
- * @package    LessPHP_Util_String
- * @copyright  Copyright 2013 HOOTO.COM
- * @license    http://www.apache.org/licenses/LICENSE-2.0
- */
 class LessPHP_Util_String
 {
     public static function cutstr($string, $length, $dot = ' ...') 
